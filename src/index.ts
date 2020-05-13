74dd86ea4583d9ee772cb7064bd368dcc849f2e9
Pierre <19956672+iwa@users.noreply.github.com>
2020-05-13 22:35:53 +0200

    add route: pm2 app check

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,19 @@
-import * as express from "express";
+import * as express from 'express';
+import * as shell from 'shelljs';
 
-import * as dotenv from "dotenv";
+import * as dotenv from 'dotenv';
 dotenv.config();
 
 const app = express();
 const port = process.env.PORT || 8080;
 
-
+app.get('/api/:app', async function(req, res) {
+    let pid = shell.exec(`pm2 pid ${req.params.app}`, {silent: true});
+    if(pid.stdout == '0' || pid.stdout == '\n' || pid.stdout.startsWith("[PM2]"))
+        return res.sendStatus(404)
+    else
+        return res.sendStatus(200)
+});
 
 app.use((req, res) => {
     res.sendStatus(404);
